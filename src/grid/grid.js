9140494bdc6ca0c24633a26365471ba29845ff23
PENNAMEN Adrien <adrien.pennamen@soprasteria.com>
2019-05-27 17:19:49 +0200

    feat(grid): introducing templates and better css grid management

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,49 +1,51 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+        --grid-width: 10;
+    }
+    
+    .wrapper {
+      display: grid;
+      grid-template-columns: repeat(var(--grid-width), 1fr);
+      grid-gap: 0;
+      grid-auto-rows: minmax(50px, auto);
+    }
+    
+    .elem {
+      border: 1px dashed grey;
+      cursor: pointer;
+    }
+  </style>
+  <div class="wrapper"></div>
+`;
+
 export default class GameGrid extends HTMLElement {
   constructor() {
     super();
     // Shadow root element
-    this.shadow = this.attachShadow({mode: 'open'});
-
-    // Wrapper of the grid
-    this.wrapper = document.createElement('div');
-    this.wrapper.setAttribute('class', 'wrapper');
-
-    const elem1 = document.createElement('div');
-    elem1.innerHTML = 'Elem 1';
-    elem1.setAttribute('class', 'elem');
-    this.wrapper.appendChild(elem1);
-    const elem2 = document.createElement('div');
-    elem2.setAttribute('class', 'elem');
-    elem2.innerHTML = 'Elem 2';
-    this.wrapper.appendChild(elem2);
-    const elem3 = document.createElement('div');
-    elem3.innerHTML = 'Elem 3';
-    elem3.setAttribute('class', 'elem');
-    this.wrapper.appendChild(elem3);
-
-    const style = document.createElement('style');
-    style.textContent =`
-      .wrapper {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 10px;
-        grid-auto-rows: minmax(100px, auto);
-      }
-      
-      .elem {
-        border: 1px solid red;
-      }
-    `;
-
-    this.shadow.appendChild(style);
-    this.shadow.appendChild(this.wrapper);
+    this._shadowRoot = this.attachShadow({mode: 'open'});
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
     const width = this.getAttribute('width');
     const height = this.getAttribute('height');
 
-    console.log(`Width; ${width}`);
-    console.log(`Height; ${height}`);
+    const $wrapper = this._shadowRoot.querySelector('.wrapper');
+
+    const hostStyle = this._shadowRoot.querySelector('style');
+    hostStyle.innerHTML += `
+      :host {
+          --grid-width: ${height};
+      }
+    `;
+
+    for (let i = 1; i <= width * height; i++) {
+      const elem = document.createElement('div');
+      elem.innerHTML = i;
+      elem.setAttribute('class', 'elem');
+      $wrapper.appendChild(elem);
+    }
   }
 }
