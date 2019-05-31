be4811055df628967f25550c6ddce3f4f4b7e7a8
PENNAMEN Adrien <adrien.pennamen@soprasteria.com>
2019-05-31 23:25:01 +0200

    doc(sprites): add documentation to component

--- src/components/sprite/draggable-sprite.js
+++ src/components/sprite/draggable-sprite.js
@@ -14,6 +14,20 @@ template.innerHTML = `
   <img class="draggable-sprite" draggable="true" />  
 `;
 
+/**
+ * Draggable sprite, basically an image that put its html into the drag event
+ *
+ * @param {string] sprite - url of the sprite
+ *
+ * import DraggableSprite from 'path/to/draggable-sprite';
+ * window.customElements.define('draggable-sprite', DraggableSprite);
+ *
+ * <draggable-sprite sprite="url/to/sprite"></draggable-sprite>
+ *
+ *
+ * On the element that receives the draggable sprite, use:
+ * this.innerHtml = e.dataTransfer.getData("text/html");
+ */
 export default class DraggableSprite extends HTMLElement {
   static get observedAttributes() {
     return ['sprite'];
@@ -21,26 +35,16 @@ export default class DraggableSprite extends HTMLElement {
 
   constructor() {
     super();
-    // Shadow root element
     this._shadowRoot = this.attachShadow({mode: 'open'});
     this._shadowRoot.appendChild(template.content.cloneNode(true));
     this.$img = this._shadowRoot.querySelector('img');
   }
 
   connectedCallback() {
+    this.addEventListener('dragstart', (e) => {
+      e.dataTransfer.setData("text/html", this._shadowRoot.innerHTML);
+    });
     this._upgradeProperty('sprite');
-  }
-
-  attributeChangedCallback(name, _, newValue) {
-    switch (name) {
-      case 'sprite':
-        // Handles side effect
-        this.$img.setAttribute('src', newValue);
-        this.addEventListener('dragstart', (e) => {
-          e.dataTransfer.setData("text/html", this._shadowRoot.innerHTML);
-        });
-        break;
-    }
   }
 
   disconnectedCallback() {
@@ -50,6 +54,14 @@ export default class DraggableSprite extends HTMLElement {
   set sprite(url) {
     // Reflects property to attribute
     this.setAttribute('sprite', url);
+  }
+
+  attributeChangedCallback(name, _, newValue) {
+    switch (name) {
+      case 'sprite':
+        this.$img.setAttribute('src', newValue);
+        break;
+    }
   }
 
   _upgradeProperty(prop) {
